376b78b8698295199ab33002644a4aa79ca693fe
itsimoni <115569994+itsimoni@users.noreply.github.com>
2026-02-04 12:56:50 +0100

    Updated package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbo: false,    // Disable Turbopack
+    useWasmBinary: true,
   },
   typescript: {
     ignoreBuildErrors: true,
